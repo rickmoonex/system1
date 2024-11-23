b8d14bec8715d4129cf6bf9e8e6ae405189b2682
rickmoonex <rick@moonen.network>
2024-11-23 16:35:17 +0100

    hw config

--- hosts/default/hardware-configuration.nix
+++ hosts/default/hardware-configuration.nix
@@ -33,6 +33,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-962005bf32a8.useDHCP = lib.mkDefault true;
   # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
