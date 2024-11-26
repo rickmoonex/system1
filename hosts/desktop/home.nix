18ebb6a37ed367312c36b98799e97a7ad6ad1f0c
rickmoonex <rick@moonen.network>
2024-11-26 23:36:53 +0100

    add obs

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -121,4 +121,12 @@ in
   };
 
   programs.home-manager.enable = true;
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
 }
