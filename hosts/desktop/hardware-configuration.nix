0656e3a52c3b43b4fd6904c12b74831a28d44315
rickmoonex <rick@moonen.network>
2024-11-30 19:10:41 +0100

    hw

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -18,15 +18,20 @@
       fsType = "ext4";
     };
 
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/BEF1-BF83";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
   fileSystems."/home/rick/projects" =
     { device = "/dev/disk/by-uuid/b87b3051-cba2-48b9-8a26-e71c8234bf62";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/BEF1-BF83";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
+  fileSystems."/var/lib/docker/overlay2/5815228bad867b3486b41460c6bf9d18bd90748e90a0e36f8ea6baa63e1a4796/merged" =
+    { device = "overlay";
+      fsType = "overlay";
     };
 
   swapDevices =
@@ -38,9 +43,12 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-22c41ef975d2.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-8a24f0b6a5d6.useDHCP = lib.mkDefault true;
   # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.veth1674359.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
